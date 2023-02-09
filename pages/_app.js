971ba90c2ab9d698769b8884e58fb9f987e8421e
emart <emart@emartui-MacBookPro-49.local>
2023-02-09 15:15:15 +0900

    setting layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import '@/styles/globals.css'
 
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+import Footer from "../src/component/Footer";
+import Top from "../src/component/Top";
+
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <div style={{ width: "100%", margin: "0 auto" }}>
+      <Top />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
