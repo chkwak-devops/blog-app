dcf19064c7a633a026bed4e8018096539a8047e0
곽채화 <chime@baggihyeon-ui-MacBook-Pro.local>
2023-03-05 16:30:34 +0900

    modify github api

--- pages/api/github.js
+++ pages/api/github.js
@@ -1,72 +1,73 @@
-import fs from 'fs'
+import fs from "fs";
 import axios from "axios";
-import base64 from 'base-64';
-import moment from 'moment';
+import base64 from "base-64";
+import moment from "moment";
 
 export default function handler(req, res) {
+  //   const GITHUB_API_KEY = "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";
+  const GITHUB_API_KEY = "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";
 
-    const GITHUB_API_KEY = "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";
+  if (req.method !== "POST") {
+    res.status(405).send({ message: "Only POST requests allowed" });
+    return;
+  }
 
+  const body = req.body;
+  console.log("-----------");
+  console.log(body);
+  console.log("-----------");
 
-    if (req.method !== 'POST') {
-        res.status(405).send({ message: 'Only POST requests allowed' })
-        return
-    }
+  let blog_title = body.title;
+  let blog_body = body.blog_body;
 
-    const body = req.body;
-    console.log(body);
+  //   let blog_title = "test-title";
+  //   let blog_body = "test-body";
 
-    let blog_title = body.title;
-    let blog_body = body.blog_body;
-    blog_title = blog_title.replace(/\s/g, '_');
-    const file_name = moment().subtract(1, 'day').format('YYYY-MM-DD') + "-" + blog_title;
+  blog_title = blog_title.replace(/\s/g, "_");
+  const file_name =
+    moment().subtract(1, "day").format("YYYY-MM-DD") + "-" + blog_title + ".md";
 
+  fs.writeFileSync(file_name, blog_body);
 
-    fs.writeFileSync(file_name, blog_body);
+  let upload_file = fs.readFileSync(file_name, "utf-8").toString();
+  console.log(upload_file);
 
-    let upload_file = fs.readFileSync(file_name).toString();
-    console.log(upload_file);
+  const content = Buffer.from(upload_file).toString("base64");
+  //   const content = base64.encode(upload_file);
+  console.log(content);
 
-    const content = base64.encode(upload_file);
-    console.log(content);
+  const data = {
+    message: `posting ${file_name}`,
+    committer: { name: "chkwak", email: "chkwak@emart.com" },
+    content: `${content}`,
+  };
 
-    const data = {
-        "message": `posting ${file_name}`,
-        "committer": { "name": "chkwak", "email": "chkwak@emart.com" },
-        "content": `${content}`
-    };
+  const config = {
+    method: "put",
+    url: `https://api.github.com/repos/chkwak-devops/chkwak-devops.github.io/contents/_posts/${file_name}`,
+    headers: {
+      Authorization: `Bearer ${GITHUB_API_KEY}`,
+      "Content-Type": "application/json",
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+    data: data,
+  };
 
-    const config = {
-        method: 'put',
-        url: `https://api.github.com/repos/chkwak-devops/chkwak-devops.github.io/contents/_posts/${file_name}`,
-        headers: {
-            'Authorization': `Bearer ${GITHUB_API_KEY}`,
-            'Content-Type': 'application/json',
-            'X-GitHub-Api-Version': '2022-11-28'
-        },
-        data: data
-    };
+  console.log("-----------------");
+  console.log(config);
+  console.log("-----------------");
 
-    console.log("-----------------");
-    console.log(data);
-    console.log("-----------------");
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
 
-    axios(config)
-        .then(function (response) {
-            console.log(JSON.stringify(response.data));
-
-            if (fs.existsSync(fine_name)) {
-                fs.unlinkSync(fine_name);
-            }
-            res.status(200).json(response.data)
-
-
-        })
-        .catch(function (error) {
-            console.log(error);
-            res.status(500).json(error)
-        });
-
-
-
+      if (fs.existsSync(file_name)) {
+        fs.unlinkSync(file_name);
+      }
+      res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.status(500).json(error);
+    });
 }
--- pages/main.js
+++ pages/main.js
@@ -3,208 +3,239 @@ import { useEffect, useState } from "react";
 
 import Head from "next/head";
 import {
-    Table,
-    Button,
-    Dimmer,
-    Loader,
-    Form,
-    Divider,
-    Input,
-    Modal,
-    TextArea,
-    Icon,
-    Header,
-    Segment,
-    Grid,
-    Search,
-    Message,
-    Sidebar,
-    Menu,
+  Table,
+  Button,
+  Dimmer,
+  Loader,
+  Form,
+  Divider,
+  Input,
+  Modal,
+  TextArea,
+  Icon,
+  Header,
+  Segment,
+  Grid,
+  Search,
+  Message,
+  Sidebar,
+  Menu,
 } from "semantic-ui-react";
 import commonStyles from "/src/component/css/Common.module.css";
 import * as backendAPI from "/src/component/BackendAPI";
 import * as commonUtil from "/src/component/Common";
 
-import { PageTitle } from "/src/component/PageTitle"
-
+import { PageTitle } from "/src/component/PageTitle";
 
 export const setOpenSideBarAction = () => {
-    setOpenSideBar(true);
-}
-
-
+  setOpenSideBar(true);
+};
 
 export default function Main() {
+  const DEFAULT_API_KEY = "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";
 
-    const DEFAULT_API_KEY = "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";
+  // const gpt_api_key = "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";
+  let gpt_api_key =
+    typeof window !== "undefined"
+      ? localStorage.getItem("CHATGPT_API_KEY")
+      : "";
 
-    // const gpt_api_key = "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";
-    let gpt_api_key = typeof window !== 'undefined' ? localStorage.getItem("CHATGPT_API_KEY") : "";
+  // const test_blog_body =
+  //   "---\n" +
+  //   "layout: single" +
+  //   'title: "블로그 포스팅 테스트입니다"\n' +
+  //   "categories: 블로그\n" +
+  //   "tag: [블로그, 코딩, 지킬, 코드스테이츠]\n" +
+  //   "toc: true\n" +
+  //   "toc_sticky: true\n" +
+  //   "toc_label: 목차\n" +
+  //   "---\n" +
+  //   "\n" +
+  //   "## 테스트\n";
 
-    const [isLoading, setIsLoading] = useState(true);
-    const [chatGPTResult, setChatGPTResult] = useState();
-    const [valueReqQuery, setValueReqQuery] = useState('');
-    const [chatfGPTAPI, setChatfGPTAPI] = useState('');
-    const [valueTopic, setValueTopic] = useState('2013 hit song');
+  const test_blog_body =
+    "---\n" +
+    "layout: single\n" +
+    'title: "test title"\n' +
+    "categories: blog\n" +
+    "tag: [blog, coding,]\n" +
+    "toc: true\n" +
+    "toc_sticky: true\n" +
+    "toc_label: index\n" +
+    "---\n" +
+    "\n" +
+    "## 한글 테스트\n";
 
+  const [isLoading, setIsLoading] = useState(true);
+  const [chatGPTResult, setChatGPTResult] = useState(test_blog_body);
+  const [valueReqQuery, setValueReqQuery] = useState("");
+  const [chatfGPTAPI, setChatfGPTAPI] = useState();
+  const [valueTopic, setValueTopic] = useState("2013 hit song");
 
-    const handleRunGPTAPI = async () => {
+  const handleRunGPTAPI = async () => {
+    setIsLoading(true);
 
-        setIsLoading(true);
+    const prompt_val = valueReqQuery.replace(/(?:\r\n|\r|\n)/g, " "); // 공백 개행 처리
+    const response = await backendAPI.runGPTAPICall(
+      prompt_val,
+      DEFAULT_API_KEY
+    );
+    if (response.status === 401) {
+      alert("인증키가 잘못되었습니다. 인증키를 재등록해주세요");
+      // TODO 인증키 등록용 모달 폼 생성
+    } else {
+      // setChatGPTResult(response.data.choices[0].text);
 
-        const prompt_val = valueReqQuery.replace(/(?:\r\n|\r|\n)/g, ' '); // 공백 개행 처리     
-        const response = await backendAPI.runGPTAPICall(prompt_val, DEFAULT_API_KEY);
-        if (response.status === 401) {
-            alert('인증키가 잘못되었습니다. 인증키를 재등록해주세요')
-            // TODO 인증키 등록용 모달 폼 생성     
-        } else {
-            // setChatGPTResult(response.data.choices[0].text);
+      let result_data = response.data.choices[0].text;
+      result_data = result_data.replace(/(?:\r\n|\r|\n)/g, " ");
+      setChatGPTResult(response.data.choices[0].text);
+    }
 
-            let result_data = response.data.choices[0].text;
-            result_data = result_data.replace(/(?:\r\n|\r|\n)/g, ' ');
-            setChatGPTResult(response.data.choices[0].text);
-        }
+    setIsLoading(false);
+  };
 
-        setIsLoading(false);
+  const handleDownloadChatGPT = async () => {
+    const fimeNameFooter = valueTopic.replace(/ /g, "_");
+    const today_str = commonUtil.getToday();
 
+    let fileName = `${today_str}_${fimeNameFooter}.md`;
+    let output = chatGPTResult;
+    const element = document.createElement("a");
+    const file = new Blob([output], {
+      type: "text/plain",
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = fileName;
+    document.body.appendChild(element); // FireFox
+    element.click();
+  };
+
+  const handleDeployBlog = async () => {
+    console.log("handleDeployBlog");
+
+    setIsLoading(true);
+
+    const reqGithubData = {
+      title: valueTopic,
+      blog_body: chatGPTResult,
     };
 
-    const handleDownloadChatGPT = async () => {
+    console.log(reqGithubData);
 
-        const fimeNameFooter = valueTopic.replace(/ /g, "_");
-        const today_str = commonUtil.getToday();
+    const response = await backendAPI.githubAPICall(reqGithubData);
 
-        let fileName = `${today_str}_${fimeNameFooter}.md`;
-        let output = chatGPTResult;
-        const element = document.createElement('a');
-        const file = new Blob([output], {
-            type: 'text/plain',
-        });
-        element.href = URL.createObjectURL(file);
-        element.download = fileName;
-        document.body.appendChild(element); // FireFox
-        element.click();
+    if (response.status !== 200) {
+      alert(`${response.status}\n오류가 발생했습니다}`);
+    } else {
+      alert("처리 완료되었습니다.");
     }
 
+    setIsLoading(false);
 
-    const handleDeployBlog = async () => {
+    // console.log(objectWithData);
 
-        console.log("handleDeployBlog");
+    // fetch("/api/github", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(objectWithData),
+    // });
 
-        const objectWithData = { "title": valueTopic, "blog_body": chatGPTResult };
+    //     let file = fs.readFileSync("/test.md").toString();
+    //     console.log(file);
 
+    //     var content = base64.encode(file);
+    //     console.log(content);
 
-        fetch('/api/github', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(objectWithData),
-        })
+    //     // uploadFileApi(token, content)
 
+    //     const github_token = "1111";
 
+    //     const data = JSON.stringify({
+    //         "message": "txt file",
+    //         "content": `${content}`
+    //     });
 
+    //     const config = {
+    //         method: 'put',
+    //         url: 'https://api.github.com/repos/chkwak-devops/chkwak-devops.github.io/_posts/2023-03-02-test.md',
+    //         headers: {
+    //             'Authorization': `Bearer ${github_token}`,
+    //             'Content-Type': 'application/json'
+    //         },
+    //         data: data
+    //     };
 
+    //     axios(config)
+    //         .then(function (response) {
+    //             console.log(JSON.stringify(response.data));
+    //         })
+    //         .catch(function (error) {
+    //             console.log(error);
+    //         });
+  };
 
-        //     let file = fs.readFileSync("/test.md").toString();
-        //     console.log(file);
+  useEffect(() => {
+    setIsLoading(false);
 
-        //     var content = base64.encode(file);
-        //     console.log(content);
-
-        //     // uploadFileApi(token, content)
-
-
-        //     const github_token = "1111";
-
-        //     const data = JSON.stringify({
-        //         "message": "txt file",
-        //         "content": `${content}`
-        //     });
-
-
-        //     const config = {
-        //         method: 'put',
-        //         url: 'https://api.github.com/repos/chkwak-devops/chkwak-devops.github.io/_posts/2023-03-02-test.md',
-        //         headers: {
-        //             'Authorization': `Bearer ${github_token}`,
-        //             'Content-Type': 'application/json'
-        //         },
-        //         data: data
-        //     };
-
-        //     axios(config)
-        //         .then(function (response) {
-        //             console.log(JSON.stringify(response.data));
-        //         })
-        //         .catch(function (error) {
-        //             console.log(error);
-        //         });
-
-    }
-
-
-
-
-
-
-    useEffect(() => {
-        setIsLoading(false);
-
-        setChatfGPTAPI(localStorage.getItem("CHATGPT_API_KEY") === "" ? DEFAULT_API_KEY : localStorage.getItem("CHATGPT_API_KEY"));
-
-    }, []);
-
-
-    return (
-        <div className={commonStyles.body_layout}>
-            <PageTitle title="ChatGPT" />
-            {isLoading && (
-                <div style={{ padding: "300px 0" }}>
-                    <Dimmer active>
-                        <Loader size="huge" inline="centered" active>
-                            Loading
-                        </Loader>
-                    </Dimmer>
-                </div>
-            )}
-
-            {!isLoading && (
-
-                <div className={commonStyles.body_layout}>
-                    <Form>
-                        <Form.Group widths='equal'>
-                            <Form.Field
-                                id='form-prompt'
-                                control={TextArea}
-                                label='ChatGPT Prompt'
-                                placeholder="ChatGPT 질의문 입력"
-                                style={{ minHeight: 200 }}
-                                onChange={(e) => setValueReqQuery(e.currentTarget.value)}
-                                value={valueReqQuery}
-                            />
-                        </Form.Group>
-                    </Form>
-                    <Segment>
-                        <Button onClick={handleRunGPTAPI}> chatGPT 질의</Button>
-                    </Segment>
-                    <Form>
-                        <TextArea style={{ minHeight: 400 }} value={chatGPTResult} />
-                    </Form>
-
-                    <Divider />
-
-                    <Segment>
-                        <Button onClick={() => handleDownloadChatGPT()}> 파일 다운로드 </Button>
-
-                        {/* https://docs.github.com/ko/rest/repos/contents?apiVersion=2022-11-28#create-a-file */}
-                        <Button onClick={() => handleDeployBlog()}> 블로그 배포</Button>
-                        <Button> 자동 블로그 배포 시간 설정 </Button>
-                        <Button> 배포 현황 조회 </Button>
-                    </Segment>
-
-                </div>
-            )}
-        </div>
+    setChatfGPTAPI(
+      localStorage.getItem("CHATGPT_API_KEY") === ""
+        ? DEFAULT_API_KEY
+        : localStorage.getItem("CHATGPT_API_KEY")
     );
+  }, []);
+
+  return (
+    <div className={commonStyles.body_layout}>
+      <PageTitle title="ChatGPT" />
+      {isLoading && (
+        <div style={{ padding: "300px 0" }}>
+          <Dimmer active>
+            <Loader size="huge" inline="centered" active>
+              Loading
+            </Loader>
+          </Dimmer>
+        </div>
+      )}
+
+      {!isLoading && (
+        <div className={commonStyles.body_layout}>
+          <Form>
+            <Form.Group widths="equal">
+              <Form.Field
+                id="form-prompt"
+                control={TextArea}
+                label="ChatGPT Prompt"
+                placeholder="ChatGPT 질의문 입력"
+                style={{ minHeight: 200 }}
+                onChange={(e) => setValueReqQuery(e.currentTarget.value)}
+                value={valueReqQuery}
+              />
+            </Form.Group>
+          </Form>
+          <Segment>
+            <Button onClick={handleRunGPTAPI}> chatGPT 질의</Button>
+          </Segment>
+          <Form>
+            <TextArea style={{ minHeight: 400 }} value={chatGPTResult} />
+          </Form>
+
+          <Divider />
+
+          <Segment>
+            <Button onClick={() => handleDownloadChatGPT()}>
+              {" "}
+              파일 다운로드{" "}
+            </Button>
+
+            {/* https://docs.github.com/ko/rest/repos/contents?apiVersion=2022-11-28#create-a-file */}
+            <Button onClick={() => handleDeployBlog()}> 블로그 배포</Button>
+            <Button> 자동 블로그 배포 시간 설정 </Button>
+            <Button> 배포 현황 조회 </Button>
+          </Segment>
+        </div>
+      )}
+    </div>
+  );
 }
