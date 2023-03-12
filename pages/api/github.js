a76c87fe4ca7dda6a867146b5f087e62aecbec1c
곽채화 <chime@baggihyeon-ui-MacBook-Pro.local>
2023-03-12 21:05:35 +0900

    modify lowdb

--- pages/api/github.js
+++ pages/api/github.js
@@ -4,75 +4,79 @@ import base64 from "base-64";
 import moment from "moment";
 
 export default function handler(req, res) {
-    //   const GITHUB_API_KEY = "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";
-    //   const GITHUB_API_KEY = "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";
-    //   const GITHUB_API_KEY = "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";
-    //git remote add origin https://chkwak-devops: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@github.com/chkwak-devops/blog-app.git
+  //   const GITHUB_API_KEY = "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";
+  //   const GITHUB_API_KEY = "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";
+  //   const GITHUB_API_KEY = "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";
+  //git remote add origin https://chkwak-devops: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@github.com/chkwak-devops/blog-app.git
 
+  if (req.method !== "POST") {
+    res.status(405).send({ message: "Only POST requests allowed" });
+    return;
+  }
 
-    if (req.method !== "POST") {
-        res.status(405).send({ message: "Only POST requests allowed" });
-        return;
-    }
+  const body = req.body;
+  console.log("-----------");
+  console.log("body : " + JSON.stringify(body));
+  console.log("-----------");
 
-    const body = req.body;
-    console.log("-----------");
-    console.log("body : " + JSON.stringify(body));
-    console.log("-----------");
+  let blog_title = body.title;
+  let blog_body = body.blog_body;
+  let github_token = body.github_token;
+  let github_account = body.github_account;
 
-    let blog_title = body.title;
-    let blog_body = body.blog_body;
-    let github_token = body.github_token;
-    let github_account = body.github_account;
+  //   let blog_title = "test-title";
+  //   let blog_body = "test-body";
 
-    //   let blog_title = "test-title";
-    //   let blog_body = "test-body";
+  blog_title = blog_title.replace(/\s/g, "_");
+  const file_name =
+    moment().subtract(1, "day").format("YYYY-MM-DD") +
+    "-" +
+    encodeURI(blog_title) +
+    "-" +
+    Math.floor(Math.random() * 1000000).toString() +
+    ".md";
 
-    blog_title = blog_title.replace(/\s/g, "_");
-    const file_name =
-        moment().subtract(1, "day").format("YYYY-MM-DD") + "-" + encodeURI(blog_title) + "-" + Math.floor(Math.random() * 1000000).toString() + ".md";
+  fs.writeFileSync(file_name, blog_body);
 
-    fs.writeFileSync(file_name, blog_body);
+  let upload_file = fs.readFileSync(file_name, "utf-8").toString();
+  console.log(upload_file);
 
-    let upload_file = fs.readFileSync(file_name, "utf-8").toString();
-    console.log(upload_file);
+  const content = Buffer.from(upload_file).toString("base64");
+  //   const content = base64.encode(upload_file);
+  console.log(content);
 
-    const content = Buffer.from(upload_file).toString("base64");
-    //   const content = base64.encode(upload_file);
-    console.log(content);
+  const data = {
+    message: `posting ${file_name}`,
+    committer: { name: "chkwak", email: "chkwak@emart.com" },
+    content: `${content}`,
+  };
 
-    const data = {
-        message: `posting ${file_name}`,
-        committer: { name: "chkwak", email: "chkwak@emart.com" },
-        content: `${content}`,
-    };
+  const config = {
+    method: "put",
+    url: `https://api.github.com/repos/${github_account}/${github_account}.github.io/contents/_posts/${file_name}`,
+    headers: {
+      Authorization: `Bearer ${github_token}`,
+      "Content-Type": "application/json",
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+    data: data,
+  };
 
-    const config = {
-        method: "put",
-        url: `https://api.github.com/repos/${github_account}/${github_account}.github.io/contents/_posts/${file_name}`,
-        headers: {
-            Authorization: `Bearer ${github_token}`,
-            "Content-Type": "application/json",
-            "X-GitHub-Api-Version": "2022-11-28",
-        },
-        data: data,
-    };
+  console.log("-----------------");
+  console.log(config);
+  console.log("-----------------");
 
-    console.log("-----------------");
-    console.log(config);
-    console.log("-----------------");
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
 
-    axios(config)
-        .then(function (response) {
-            console.log(JSON.stringify(response.data));
-
-            if (fs.existsSync(file_name)) {
-                fs.unlinkSync(file_name);
-            }
-            res.status(200).json(response.data);
-        })
-        .catch(function (error) {
-            console.log(error);
-            res.status(error.response.status).json(error.response.data.message);
-        });
+      if (fs.existsSync(file_name)) {
+        fs.unlinkSync(file_name);
+      }
+      res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.status(error.response.status).json(error.response.data.message);
+    });
 }
--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,18 +1,38 @@
-import dayjs from 'dayjs';
-import shortId from 'shortid';
-import db from '/src/component/lowdb';
+import dayjs from "dayjs";
+import shortId from "shortid";
+import lowdb from "/src/component/lowdb";
 
 export default async function handler(req, res) {
-    const { method } = req;
+  const { method } = req;
 
-    if (method === "GET") {
-        await db.read();
+  if (method === "GET") {
+    // lowdb.get("posts").remove().write();
 
-        console.log(db.data);
-        const postsList = db.data;
-        return res.status(200).json(postsList);
-    }
+    // lowdb.get("user").remove().write();
 
+    lowdb
+      .get("posts")
+      .push({
+        id: shortId.generate(),
+        createDt: dayjs().format("YYYY-MM-DD HH:mm:ss:SSS"),
+        blog_category: "blog_category",
+        blog_title: "blog_category",
+        blog_contents: "blog_contents",
+        blog_writer: "blog_writer",
+      })
+      .write();
 
-    res.status(200).json({ name: 'John Doe' })
+    // const { posts } = db;
+    // posts.push({
+    //   id: shortId.generate(),
+    //   date: dayjs().format("YYYY-MM-DD HH:mm:ss:SSS"),
+    //   done: false,
+    // });
+    // await db.write();
+    // await db.read();
+    // console.log(posts);
+    return res.status(200).json(lowdb);
+  }
+
+  // res.status(200).json({ name: 'John Doe' })
 }
